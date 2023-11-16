84605e440a073e2606b6a337b0bd1837fcb28a21
moeyy01 <57945487+moeyy01@users.noreply.github.com>
2023-11-16 08:10:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 var static = require("node-static");
-
 var file = new static.Server(".");
-PORT = 8080;
+var PORT = 8080;
 
 console.info(`Running at http://localhost:${PORT}`);
 
 require("http")
   .createServer(function (request, response) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+
+    if (request.method === "OPTIONS") {
+      response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+      response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      response.writeHead(200);
+      response.end();
+      return;
+    }
+
     request
       .addListener("end", function () {
         file.serve(request, response);
